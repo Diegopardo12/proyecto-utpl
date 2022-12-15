c180231aadda0160e2148b2aa27343d89383c19c
Diegopardo12 <dapardo6@utpl.edu.ec>
2022-12-15 13:56:20 -0500

    asistencia

---
+++ app/src/main/java/com/example/proyecto_utpl/asistencia.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_utpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class asistencia : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_asistencia)
+    }
+}
