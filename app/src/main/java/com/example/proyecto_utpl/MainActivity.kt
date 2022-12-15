cb38613942989ad06e89be9f421a75b2bc97e456
Diegopardo12 <dapardo6@utpl.edu.ec>
2022-12-15 11:20:34 -0500

    Initial commit

---
+++ app/src/main/java/com/example/proyecto_utpl/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.example.proyecto_utpl
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+    @Override
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+                // Inicia un temporizador para mostrar la pantalla de inicio durante 2 segundos
+                val timer = Timer()
+                timer.schedule(object : TimerTask() {
+                    override fun run() {
+                        // Cuando el temporizador finalice, inicia la siguiente actividad
+                        startActivity(Intent(this@MainActivity, principal::class.java))
+                        finish()
+                    }
+                }, 2000)
+            }
+
+
+
+    }
