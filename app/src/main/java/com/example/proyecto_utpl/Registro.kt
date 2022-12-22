035be89c2e1d5b0b42c3251e584714634eaae861
Bryan Rodriguez <barodriguez8@utpl.edu.ec>
2022-12-22 11:46:24 -0500

    Registro de usuarios y datos en Firebase

---
+++ app/src/main/java/com/example/proyecto_utpl/Registro.kt
@@ -0,0 +1,47 @@
+package com.example.proyecto_utpl
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_registro.*
+import kotlinx.android.synthetic.main.activity_asistencia.*
+
+enum class ProviderType{
+    Personal,
+}
+
+class Registro : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+
+        val bundle:Bundle? = intent.extras
+        val email:String? = bundle?.getString("email")
+        val provider:String? = bundle?.getString("provider")
+
+        setup(email?:"", provider?:"")
+
+    }
+    private fun setup(email: String, provider:String){
+
+        tv_email.text= email
+        tv_provider.text= provider
+
+        btn_registro.setOnClickListener(){
+
+            db.collection("usuarios").document(email).set(
+                hashMapOf(
+                    "provider" to provider,
+                    "nombre" to et_nombre.text.toString(),
+                    "apellido" to et_lastname.text.toString()
+                )
+            )
+
+            val salto: Intent = Intent(this,Home::class.java)
+            startActivity(salto)
+        }
+    }
+}
--- app/src/main/java/com/example/proyecto_utpl/asistencia.kt
+++ app/src/main/java/com/example/proyecto_utpl/asistencia.kt
@@ -1,11 +1,71 @@
 package com.example.proyecto_utpl
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_asistencia.*
 
 class asistencia : AppCompatActivity() {
+    private val db = FirebaseFirestore.getInstance()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_asistencia)
+        setup()
+    }
+    private fun setup() {
+        btn_ingresar.setOnClickListener {
+            if(et_email.text.isNotEmpty() && et_clave.text.isNotEmpty()){
+                FirebaseAuth.getInstance().createUserWithEmailAndPassword(
+                    et_email.text.toString(),
+                    et_clave.text.toString()).addOnCompleteListener{
+                        if (it.isSuccessful){
+                            showHome(it.result?.user?.email?:"", ProviderType.Personal)
+                        }else{
+                            showAlert()
+                        }
+                }
+            }
+        }
+
+        btn_acceder.setOnClickListener {
+            if(et_email.text.isNotEmpty() && et_clave.text.isNotEmpty()){
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                    et_email.text.toString(),
+                    et_clave.text.toString()).addOnCompleteListener{
+                    if (it.isSuccessful){
+                        showHome2(it.result?.user?.email?:"", ProviderType.Personal)
+                    }else{
+                        showAlert()
+                    }
+                }
+            }
+        }
+
+    }
+    private fun showAlert() {
+        val builder = AlertDialog.Builder(this)
+        builder.setTitle("Error")
+        builder.setMessage("Por favor registrese antes de continuar ")
+        builder.setPositiveButton("Aceptar", null)
+        val dialog: AlertDialog = builder.create()
+        dialog.show()
+    }
+    private fun showHome(email:String, provider: ProviderType){
+        val homeIntent: Intent = Intent(this,Registro::class.java).apply {
+            putExtra("email",email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
+    }
+
+    private fun showHome2(email:String, provider: ProviderType){
+        val homeIntent: Intent = Intent(this,Home::class.java).apply {
+            putExtra("email",email)
+            putExtra("provider", provider.name)
+        }
+        startActivity(homeIntent)
     }
 }
