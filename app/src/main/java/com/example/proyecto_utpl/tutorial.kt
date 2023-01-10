4f61af52c04d507146d79054684ac3ccefcdc5ad
Diegopardo12 <dapardo6@utpl.edu.ec>
2023-01-10 09:20:30 -0500

    Holaaaa

---
+++ app/src/main/java/com/example/proyecto_utpl/tutorial.kt
@@ -0,0 +1,11 @@
+package com.example.proyecto_utpl
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class tutorial : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_tutorial)
+    }
+}
