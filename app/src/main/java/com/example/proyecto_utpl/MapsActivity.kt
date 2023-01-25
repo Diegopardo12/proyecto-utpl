f0560f6cde072ff5b31695d282033022d5833221
PabloSaraguro <pjsaraguro@utpl.edu.ec>
2023-01-25 15:56:33 -0500

    finalisimo1

--- app/src/main/java/com/example/proyecto_utpl/MapsActivity.kt
+++ app/src/main/java/com/example/proyecto_utpl/MapsActivity.kt
@@ -1,27 +1,27 @@
 package com.example.proyecto_utpl
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.R.layout.*
 import android.content.pm.PackageManager
+import android.graphics.Color
+import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-
+import com.example.proyecto_utpl.databinding.ActivityMapsBinding
+import com.google.android.gms.location.LocationServices
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
-import com.example.proyecto_utpl.databinding.ActivityMapsBinding
-import java.util.jar.Manifest
+import com.google.android.gms.maps.model.*
 
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var mMap: GoogleMap
     private lateinit var binding: ActivityMapsBinding
+    private lateinit var zone: Circle
 
     companion object{
         const val REQUEST_CODE_LOCATION = 0
@@ -44,9 +44,71 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         // Add a marker in Sydney and move the camera
         val utpl = LatLng(-3.9870386238500495, -79.19863502335933)
         mMap.addMarker(MarkerOptions().position(utpl).title("UTPL Loja"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(utpl))
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(utpl,15f))
+
+        /*
+        //ubicacion en circulo
+        val radius = 70.0
+        val strokeColor = Color.RED
+        val fillColor = Color.argb(64, 255, 0, 0)
+
+        zone = googleMap.addCircle(
+            CircleOptions()
+            .center(utpl)
+            .radius(radius)
+            .strokeColor(strokeColor)
+            .fillColor(fillColor))
+
+        */
+
+        // Crear un objeto Polygon
+        var zone: Polygon = googleMap.addPolygon(
+            PolygonOptions()
+                .add(
+                    LatLng(-3.986312, -79.199742),
+                    LatLng(-3.988364, -79.199107),
+                    LatLng(-3.988488, -79.198313),
+                    LatLng(-3.987578, -79.196934),
+                    LatLng(-3.986502, -79.196703),
+                    LatLng(-3.985379, -79.198066)
+                )
+                .strokeColor(Color.RED)
+                .fillColor(Color.argb(64, 255, 0, 0))
+        )
+
+
+        val locationClient = LocationServices.getFusedLocationProviderClient(this)
+        locationClient.lastLocation.addOnSuccessListener { location ->
+            // Verificar si la ubicación actual está dentro de la zona
+            val currentLocation = LatLng(location.latitude, location.longitude)
+            val isInside = zone.contains(currentLocation)
+
+            if (isInside) {
+                // Está dentro de la zona
+
+
+            } else {
+                // No está dentro de la zona
+
+
+            }
+        }
+
+
         enableLocation() //Activacion de la Localizacion
     }
+
+
+    /*
+
+    //comprobar si está dentro de la universidad
+
+    private fun checkIfInsideZone(location: LatLng): Boolean {
+        return zone.contains(location)
+    }
+    */
+
+
 
     private fun isLocationPermissionGranted()= ContextCompat.checkSelfPermission(
         this, android.Manifest.permission.ACCESS_FINE_LOCATION
@@ -97,4 +159,14 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     }
 
 
+
+
+
+
+
+
+}
+
+private fun Polygon.contains(currentLocation: LatLng): Boolean {
+return true
 }
